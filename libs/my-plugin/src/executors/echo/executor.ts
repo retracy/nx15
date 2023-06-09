aefe5371659cfcc3f2516c50c6b2f2b7a53b962c
Bob Tracy <retracy@live.com>
2023-06-09 11:26:31 -0700

    Create data folder

--- libs/my-plugin/src/executors/echo/executor.ts
+++ libs/my-plugin/src/executors/echo/executor.ts
@@ -1,5 +1,6 @@
 import { EchoExecutorSchema } from './schema';
 import * as fs from 'fs';
+import * as path from 'path';
 
 export default async function runExecutor(
   options: EchoExecutorSchema,
@@ -10,7 +11,9 @@ export default async function runExecutor(
     // date: new Date().toDateString(),
     time: new Date().toLocaleTimeString()
   };
-  fs.writeFileSync("test_results/ruffle-data/ruffle-report.json", JSON.stringify(data));
+  const dir = "test_results/ruffle-data";
+  fs.mkdirSync(dir, { recursive: true });
+  fs.writeFileSync(path.join(dir, "ruffle-report.json"), JSON.stringify(data));
 
   return {
     success: true
